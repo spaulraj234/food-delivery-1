744432853c0ac56c241d83a31bce142a880fbe8e
spaulraj234 <scute8635@gmail.com>
2025-08-24 20:27:28 +0530

    update all

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-<!doctype html>
+
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Food Delivering</title>
-    <link rel="stylesheet" href="./src/components/context/pages">
+   
 
   </head>
   <body>
